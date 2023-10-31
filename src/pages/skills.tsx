199454278a9864083ff33508bda68c47baae8bc2
Vlad Polevoi <vlad.polewoi1@gmail.com>
2023-10-31 22:08:40 +0200

    feat: :sparkles: styles and variables

---
+++ src/pages/skills.tsx
@@ -0,0 +1,4 @@
+const skills = [
+"Javascript",
+"vue", "react", "node", "express", "mongodb", "sql", "html", "css", "sass", "bootstrap", "tailwind", "git", "github", "figma", "adobe xd", "adobe photoshop", "adobe illustrator", "adobe indesign", "adobe after effects", "adobe premiere pro", "adobe audition", "adobe animate", "adobe lightroom", "adobe xd", "adobe dimension", "adobe acrobat", "adobe bridge", "adobe media encoder", "adobe incopy", "adobe fuse", "adobe char"
+];
