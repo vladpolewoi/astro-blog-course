01e0604248895436225f23e7a31037d11ed63d0e
Vlad Polevoi <vlad.polewoi1@gmail.com>
2023-11-02 20:50:08 +0200

    feat: :sparkles: rss + tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from "astro/config"
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    devOverlay: true
-  },
-  integrations: [tailwind()]
-});
+	site: "https://astro-blog-course.netlify.app/",
+	experimental: {
+		devOverlay: true,
+	},
+	integrations: [tailwind()],
+})
